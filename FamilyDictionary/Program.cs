4420b652b29fc038205d4a018d5615010bfe984c
Amanda Slayton <alslayton86@gmail.com>
2018-09-11 17:55:05 -0500

    finished

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,39 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+            myFamily.Add("sister", new Dictionary<string, string>()
+            {
+                {"name", "Alyssa"},
+                {"age", "26"}
+            });
+            myFamily.Add("brother", new Dictionary<string, string>()
+            {
+                {"name", "Aaron"},
+                {"age", "28"}
+            });
+            myFamily.Add("father", new Dictionary<string, string>()
+            {
+                {"name", "Randall"},
+                {"age", "52"}
+            });
+            myFamily.Add("mother", new Dictionary<string, string>()
+            {
+                {"name", "Diana"},
+                {"age", "51"}
+            });
+            myFamily.Add("husband", new Dictionary<string, string>()
+            {
+                {"name", "Brandon"},
+                {"age", "34"}
+            });
+
+            foreach (var relationship in myFamily)
+            {
+                Console.WriteLine($"My {relationship.Key}'s name is {relationship.Value["name"]} and is {relationship.Value["age"]} years old.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
